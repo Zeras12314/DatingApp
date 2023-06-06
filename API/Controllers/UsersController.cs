afdb539661dc193ace435d3516b585ff9726dff9
Zeras <tiongson.gerson@gmail.com>
2023-06-06 15:52:49 +0800

    End of Section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -27,11 +27,9 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>>GetUsers()
         {
-            var users = await _userRepository.GetUsersAync();
-
-            var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
-
-            return Ok(usersToReturn);
+            var users = await _userRepository.GetMembersAsync();
+            
+            return Ok(users);
 
             
         }
@@ -39,9 +37,9 @@ namespace API.Controllers
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            var user = await _userRepository.GetUserByUsernameAsync(username);
+           return await _userRepository.GetMembersAsync(username);
 
-            return _mapper.Map<MemberDto>(user);
+            
             
         }
     }
--- API/Interface/IUserRepository.cs
+++ API/Interface/IUserRepository.cs
@@ -1,4 +1,5 @@
 
+using API.DTO;
 using API.Entities;
 
 namespace API.Interface
@@ -10,5 +11,7 @@ namespace API.Interface
         Task<IEnumerable<AppUser>> GetUsersAync();
         Task<AppUser> GetUserByIdAsync(int id);
         Task<AppUser> GetUserByUsernameAsync(string username);
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<MemberDto> GetMembersAsync(string username);
     }
 }
